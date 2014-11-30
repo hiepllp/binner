f1fc07dc8079c040a1aa4f5137aa7095bf97721a
nadirhamid <matrix.nad@gmail.com>
2014-11-30 11:59:18 -0700

    Initial commit

---
+++ unit.py
@@ -0,0 +1,19 @@
+"""
+Unit tests for Binner
+
+algorithms tested:
+
+MultiBinPacking
+SingleBinPacking
+SmallestBinPacking
+"""
+
+class BinnerUnitTest(unittest.TestCase):
+	def test_multi_bin(self):
+		pass
+
+	def test_single_bin(self):
+		pass
+
+	def test_smallest_bin(self):
+		pass
