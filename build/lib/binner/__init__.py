d0762e907d7038d843532f635d75b1229ffb6140
matrix.nad <matrix.nad@gmail.com>
2016-11-25 13:19:14 -0600

    add .gitignore options

--- build/lib/binner/__init__.py
+++
@@ -1,7 +0,0 @@
-import logging
-logging.basicConfig(
-	 level=logging.DEBUG,
-	 format="%(asctime)s %(message)s" )
-
-log = logging.getLogger("")
-
--- build/lib/binner/collection_bin.py
+++
@@ -1,8 +0,0 @@
-from .collection import Collection
-from .entity_bin import Bin
-
-class BinCollection(Collection):
-   def __init__(self,args):
-	 super(BinCollection,self).__init__(args)
-   def get_entity(self):
-	 return Bin
